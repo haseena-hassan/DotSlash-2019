f91dc12204e6717674ad74c3e474519fdf101bf6
haseena-hassan <haseena2199@gmail.com>
2019-03-27 21:17:06 +0530

    added logo animation

--- public/assets/js/anim.js
+++ public/assets/js/anim.js
@@ -10,30 +10,6 @@ function typeWriter() {
   }
 }
 
-// ..............ctrl + k + c
-//     let start = Date.now(); // remember start time
-
-// let timer = setInterval(function() {
-//   // how much time passed from the start?
-//   let timePassed = Date.now() - start;
-
-//   if (timePassed >= 2000) {
-//     clearInterval(timer); // finish the animation after 2 s
-//     return;
-//   }
-
-//   // draw the animation at the moment timePassed
-//   draw(timePassed);
-
-// }, 20);
-
-// // as timePassed goes from 0 to 2000
-// // left gets values from 0px to 400px
-// function draw(timePassed) {
-//   train.style.left = timePassed / 5 + 'px';
-// }
-
-
 function  moveRight() {
     var start = Date.now();
 
@@ -47,4 +23,5 @@ function  moveRight() {
 
     }, 20);
 }
- 
+
+
