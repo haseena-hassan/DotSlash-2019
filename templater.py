7d2e806df9364c2f80d30321330bb25789699416
rwithik <rwithik@gmail.com>
2019-04-07 15:43:35 +0530

    workshop page and games page

--- templater.py
+++ templater.py
@@ -1,4 +1,4 @@
-modalText = '\t<div id="myModal" class="modal fade" role="dialog">\n        <div class="modal-dialog">\n            <div class="modal-content">\n                <div class="modal-header">\n                    <button type="button" class="close" data-dismiss="modal">&times;</button>\n                    <h4 class="modal-title">Modal Header</h4>\n                </div>\n                <div class="modal-body">\n                    <p>Some text in the modal.</p>\n                </div>\n                <div class="modal-footer">\n                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>\n                </div>\n            </div>\n        </div>\n    </div>'
+modalText = '\t<div id="myModal" class="modal fade" role="dialog">\n        <div class="modal-dialog">\n            <div class="modal-content">\n                <div class="modal-header">\n                    <button type="button" class="close" data-dismiss="modal">&times;</button>\n                    <h4 class="modal-title">Modal Header</h4>\n                </div>\n                <div class="modal-body col-md-12 col-sm-12">\n                    <p>Some text in the modal.</p>\n                </div>\n                <div class="modal-footer">\n                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>\n                </div>\n            </div>\n        </div>\n    </div>'
 
 with open('modals.txt') as f:
     modals = [x[:-1] for x in f.readlines()]
@@ -22,7 +22,7 @@ for line in modals:
 
 print("\n\n\n\n\n=========Cards=========\n\n\n")
 
-cardText = '\t\t<div class="container-fluid">\n            <a data-toggle=\'modal\' data-target=\'#myModal\'>\n                <div class="row event-card">\n                    <div class="col-md-4 col-sm-12">\n                        <img class="img-responsive event-card-img" src="assets/img/pair coding.jpeg">\n                    </div>\n                    <div class="col-md-8 col-sm-12">\n                        <h1>Event Title!</h1>\n                    </div>\n                </div>\n            </a>\n        </div>'
+cardText = '\t\t<div class="container-fluid">\n            <a data-toggle=\'modal\' data-target=\'#myModal\'>\n                <div class="row event-card">\n                    <div class="col-md-4 col-sm-12">\n                        <img class="img-responsive event-card-img" src="assets/img/ethicalicon.jpeg">\n                    </div>\n                    <div class="col-md-8 col-sm-12">\n                        <h1>Event Title!</h1>\n                    </div>\n                </div>\n            </a>\n        </div>'
 for line in modals:
     if (line == "==="):
         print(cardText.replace("Event Title!", titleText).replace("myModal", titleText.replace(' ', '-') + '-modal'))
